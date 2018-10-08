9f66017ec7ab274142df99d9b18d83a373089284
Nms <37930900+Nms@users.noreply.github.com>
2018-10-08 17:06:15 +0300

    Update JuomaTestaus.java
    Testing2

---
+++ JuomaTestaus.java
@@ -0,0 +1,24 @@
+package kahvikone;
+
+public class JuomaTestaus {
+
+	public static void main(String[] args) {
+
+		JuomaAutomaatti omaKone = new JuomaAutomaatti();
+			omaKone.valmistaKahvi();
+			omaKone.valmistaKahvi();
+			omaKone.valmistaKahvi();
+	
+			omaKone.valmistaTee();
+			omaKone.valmistaTee();
+			omaKone.valmistaTee();
+			omaKone.valmistaKaakao();
+			omaKone.valmistaKaakao();
+			omaKone.valmistaKaakao();
+			omaKone.valmistaKaakao();
+			omaKone.valmistaKaakao();
+	}		
+}
+
+
+
